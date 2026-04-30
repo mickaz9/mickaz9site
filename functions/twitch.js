e9f9f7aead42c290f3af5165eead25d3229caf87
mickaz9 <micka23mar@gmail.com>
2026-05-01 01:39:34 +0200

    Update twitch.js

--- functions/twitch.js
+++ functions/twitch.js
@@ -1,13 +1,35 @@
 export async function onRequest(context) {
   const CLIENT_ID = context.env.TWITCH_CLIENT_ID;
   const CLIENT_SECRET = context.env.TWITCH_CLIENT_SECRET;
-  
+
+  // Obtenir le token
   const authRes = await fetch(`https://id.twitch.tv/oauth2/token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=client_credentials`, { method: 'POST' });
   const authData = await authRes.json();
-  
-  const twitchRes = await fetch('https://api.twitch.tv/helix/streams?user_login=mickaz9', {
-    headers: { 'Client-ID': CLIENT_ID, 'Authorization': `Bearer ${authData.access_token}` }
+  const token = authData.access_token;
+
+  const headers = { 'Client-ID': CLIENT_ID, 'Authorization': `Bearer ${token}` };
+
+  // Live stream
+  const streamRes = await fetch('https://api.twitch.tv/helix/streams?user_login=mickaz9', { headers });
+  const streamData = await streamRes.json();
+
+  // User ID pour les followers
+  const userRes = await fetch('https://api.twitch.tv/helix/users?login=mickaz9', { headers });
+  const userData = await userRes.json();
+  const userId = userData.data?.[0]?.id;
+
+  // Followers
+  let followers = null;
+  if (userId) {
+    const folRes = await fetch(`https://api.twitch.tv/helix/channels/followers?broadcaster_id=${userId}`, { headers });
+    const folData = await folRes.json();
+    followers = folData.total ?? null;
+  }
+
+  return new Response(JSON.stringify({
+    data: streamData.data || [],
+    followers: followers
+  }), {
+    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
   });
-  const data = await twitchRes.json();
-  return new Response(JSON.stringify(data), { headers: { 'Content-Type': 'application/json' } });
 }
